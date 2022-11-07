3b6b3cb6062cf585e7cb1bf374b6f76ee6c04288
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-11-07 19:26:06 -0300

    Adicionar arquivos de projeto.

---
+++ MediaDeIdade/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        string nome1, nome2;
+        int idade1, idade2;
+        double media;
+        string[] vet;
+
+        Console.WriteLine("Entre com o nome e idade do Primeiro Usuário");
+        vet = Console.ReadLine().Split(' ');
+        nome1 =  vet[0];
+        idade1 = int.Parse(vet[1]);
+
+        Console.WriteLine("Entre com o nome e idade do Segundo Usuário");
+        vet = Console.ReadLine().Split(' ');
+        nome2 = vet[0];
+        idade2 = int.Parse(vet[1]);
+
+        media = (double)(idade1 + idade2) / 2.0;
+
+        Console.WriteLine("A idade média de " + nome1 + " e " + nome2 + " é de " 
+            + media.ToString("F1", CultureInfo.InvariantCulture) + " anos ");
+        Console.ReadLine();
+
+
+        
+
+
+
+    }
+
+}
